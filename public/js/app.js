53f74c396d6b7b1637de77902a0bce37fc80f777
Juego <sharansoshetty.123@gmail.com>
2019-07-11 19:31:24 +0530

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,8 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()//it stops clearing search form after submitting
 
   const location = search.value
-
-  fetch('http://localhost:3000/weather?address='+ location +'').then((response) => {
+//fetch('http://localhost:3000/weather?address='+ location +'').then((response) => {
+  fetch('/weather?address='+ location +'').then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
